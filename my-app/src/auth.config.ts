115004ecffb809d44a63f2750d983058a7a0e37c
Misho <chapidze.misho95@gmail.com>
2024-03-24 23:40:12 +0400

    s

--- my-app/src/auth.config.ts
+++ my-app/src/auth.config.ts
@@ -7,12 +7,12 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith("/invoice");
+      const isOnDashboard = nextUrl.pathname.startsWith("/");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL("/invoice", nextUrl));
+        return Response.redirect(new URL("/", nextUrl));
       }
       return true;
     },
