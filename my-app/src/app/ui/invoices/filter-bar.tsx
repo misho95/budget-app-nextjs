dfbbaa8e98857989df9707a914770bc29010c44e
Misho <chapidze.misho95@gmail.com>
2024-03-26 01:05:18 +0400

    fxd

--- my-app/src/app/ui/invoices/filter-bar.tsx
+++ my-app/src/app/ui/invoices/filter-bar.tsx
@@ -46,46 +46,48 @@ const FilterBar = () => {
     <div className="w-full bg-[#f4f3fa] py-2 px-5 rounded-md shadow-sm shadow-black/10 mb-3">
       <form
         action={filterPostsAction}
-        className="flex flex-col md:flex-row gap-1 items-center justify-between"
+        className="flex flex-col md:flex-row gap-3 items-center justify-between"
       >
-        <label className="flex gap-1 items-center">
-          <div className="w-max">Date-From: </div>
-          <input
-            type="date"
-            className="w-full p-1 rounded-md border-[1px] focus:outline-none"
-            name="dateFrom"
-          />
-        </label>
-        <label className="flex gap-1 items-center">
-          <div>Date-To: </div>
-          <input
-            type="date"
-            className="w-full p-1 rounded-md border-[1px] focus:outline-none"
-            name="dateTo"
-          />
-        </label>
-        <label className="flex gap-1 items-center">
-          <div>Category: </div>
-          <select
-            name="category"
-            className="w-full p-1 rounded-md border-[1px] focus:outline-none"
-          >
-            <option>any</option>
-            <option>shopping</option>
-            <option>gym</option>
-          </select>
-        </label>
-        <label className="flex gap-1 items-center">
-          <div>Type: </div>
-          <select
-            name="type"
-            className="w-full p-1 rounded-md border-[1px] focus:outline-none"
-          >
-            <option>any</option>
-            <option>expense</option>
-            <option>income</option>
-          </select>
-        </label>
+        <div className="flex flex-col md:flex-row gap-5">
+          <label className="flex gap-1 items-center">
+            <div className="w-max">Date-From: </div>
+            <input
+              type="date"
+              className="w-full p-1 rounded-md border-[1px] focus:outline-none"
+              name="dateFrom"
+            />
+          </label>
+          <label className="flex gap-1 items-center">
+            <div>Date-To: </div>
+            <input
+              type="date"
+              className="w-full p-1 rounded-md border-[1px] focus:outline-none"
+              name="dateTo"
+            />
+          </label>
+          <label className="flex gap-1 items-center">
+            <div>Category: </div>
+            <select
+              name="category"
+              className="w-full p-1 rounded-md border-[1px] focus:outline-none"
+            >
+              <option>any</option>
+              <option>shopping</option>
+              <option>gym</option>
+            </select>
+          </label>
+          <label className="flex gap-1 items-center">
+            <div>Type: </div>
+            <select
+              name="type"
+              className="w-full p-1 rounded-md border-[1px] focus:outline-none"
+            >
+              <option>any</option>
+              <option>expense</option>
+              <option>income</option>
+            </select>
+          </label>
+        </div>
         <button className="bg-[#80475b] px-5 py-1 rounded-md text-white">
           Filter
         </button>
